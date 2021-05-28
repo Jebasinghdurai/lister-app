203dd4545e532caca0a327da5ba13f1a3e366078
Jebasinghdurai <jebadurai2915@gmail.com>
2021-05-28 22:12:32 +0530

    1 file modified, 1 added

---
+++ src/Category.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+import Button from 'react-bootstrap/Button';
+export default function Category() {
+  return (
+    <div>
+      <Buttons />
+    </div>
+  );
+}
